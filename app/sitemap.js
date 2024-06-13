d090f7bc231332e1380137702526a7d7e17e49bf
mohammad-jf <m.h.jalifathi@gmail.com>
2024-06-13 12:12:17 +0330

    dynamic routes site map

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,9 +1,17 @@
-export default function sitemap() {
+export default async function sitemap() {
     const staticRoutes = ['', '/users'];
     const routes = staticRoutes.map((route) => ({
         url: `http://localhost:3000${route}`,
         lastModified: new Date().toString()
     })
     );
-    return [...routes]
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    const dynamicRoutes = data.map((user) => ({
+        url: `http://localhost:3000/users/${user.id}`,
+        lastModified: new Date().toString()
+    }))
+
+    return [...routes, ...dynamicRoutes]
 }
